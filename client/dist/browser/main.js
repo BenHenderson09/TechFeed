ce104059194318c06ea2587527698e5eee8f6d41
Ben Henderson <benh090704@gmail.com>
2018-10-21 15:17:22 +0100

    changed viewport

--- client/dist/browser/main.js
+++ client/dist/browser/main.js
@@ -2247,7 +2247,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_2__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\HP\Desktop\CodingMain\Current Projects\TechFeed\client\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\HP\Desktop\CodingMain\Current Projects\TechFeed\Client\src\main.ts */"./src/main.ts");
 
 
 /***/ })
