fee5f9d6c58d095cf7a041b914b275ffdee7f77a
Ben Henderson <benh090704@gmail.com>
2018-12-24 17:37:11 +0000

    Testing ads

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  local: true,
+  production: true,
+  local: false,
   localURL: "http://localhost:3000",
   prodURL: "http://www.technologyfeed.org"
 };
