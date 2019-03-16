f5540fde3d649f0feb507f232155616ef2f4e1bd
Ben Henderson <benh090704@gmail.com>
2019-03-16 15:09:01 +0000

    Added forced https

--- client/server.ts
+++ client/server.ts
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Redirect all to https
-app.use('*', (req, res, next) => {
+app.use((req, res, next) => {
   if (req.protocol != 'https'){
     console.log("-------------------------NOT SECURE-------------------------------------");
     res.redirect(`https://${req.get('host')}${req.originalUrl}`);
